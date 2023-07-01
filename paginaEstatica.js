e1445806e34b1af8d5bddaf75326031c7cdf5380
CarlosEC29 <cuevas.carlos.emmanuel@gmail.com>
2023-07-01 10:09:14 -0300

    Estilo y programación de codigo

---
+++ paginaEstatica.js
@@ -0,0 +1,6 @@
+// Función para ENVIAR FORMULARIO si todos los campos están completos
+function comprobarCampos(event) {
+    event.preventDefault();
+}
+
+document.getElementById("miformulario").reset();
