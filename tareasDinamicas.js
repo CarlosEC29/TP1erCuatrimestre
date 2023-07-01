45007165b3b416e4a5df2eff2e87bceafdad0e9a
CarlosEC29 <cuevas.carlos.emmanuel@gmail.com>
2023-07-01 10:09:32 -0300

    Modificación de estilo

---
+++ tareasDinamicas.js
@@ -0,0 +1,41 @@
+// Click en la X para ocultar el elemento de la lista
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    var div = this.parentElement;
+    div.style.display = "none";
+  };
+}
+
+// Crea un nuevo item en la lista cuando clickeo en el boton "agregar"
+function newElement() {
+  var li = document.createElement("li");
+  li.addEventListener("click", function (ev) {
+    ev.target.classList.toggle("checked");
+  }
+  )
+
+  var inputValue = document.getElementById("miTarea").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === "") {
+    alert("Debes escribir algo para que se agregue en el listado!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("miTarea").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      var div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
+}
